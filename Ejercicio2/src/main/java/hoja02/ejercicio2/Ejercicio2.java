2ef8dfb88b40203a3e6e78595b4772c483be45a8
CPedrajo <carmen.pedrajorabago@iesmiguelherrero.com>
2024-01-12 12:02:42 +0100

    Ejercicio2

---
+++ Ejercicio2/src/main/java/hoja02/ejercicio2/Ejercicio2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hoja02.ejercicio2;
+
+/**
+ *
+ * @author carme
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
