9154b7e3ed5a6de8635072cef48526db7bebd4c7
CPedrajo <carmen.pedrajorabago@iesmiguelherrero.com>
2024-01-16 15:46:32 +0100

    consola netbeans

---
+++ UD03/Hoja04/Ejercicio4/src/main/java/hoja04/ejercicio4/Ejercicio4.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hoja04.ejercicio4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM112
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        Random r= new Random();
+        int c=r.nextInt(10000);
+        int intentos=4;
+        System.out.println("La combinacion es: "+c);
+        Scanner t;
+        
+        for (int y=1;y<=intentos;y++){
+            System.out.println("Introduce la combinacion: ");
+            int pin=new Scanner (System.in).nextInt();
+            if (pin==c){
+                System.out.println("La caja fuerte se ha abierto satisfactoriamente. ");
+                
+            }else{ 
+                System.out.println("Lo siento, esa no es la combinacion le quedan "+(intentos-y)+" intentos");
+            }
+        }
+        
+       
+        
+    }
+}
