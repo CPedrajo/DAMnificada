b6db579a2a0f6b0b43d2dfc847358b2bd208f549
CPedrajo <carmen.pedrajorabago@iesmiguelherrero.com>
2024-01-22 10:56:46 +0100

    desde netbeans

---
+++ UD4/Hoja02/Ejercicio1/src/main/java/hoja02/ejercicio1/Ejercicio1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hoja02.ejercicio1;
+
+/**
+ *
+ * @author carme
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Operaciones o =new Operaciones();
+      o.mostrar();
+       o.m1();
+       o.mostrar();
+        System.out.print(o.m2()+" "); 
+        
+        
+    }
+}
