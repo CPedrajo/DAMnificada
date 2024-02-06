e3b805f7558d77c2aa4383e9accd0263046bdee6
CPedrajo <carmen.pedrajorabago@iesmiguelherrero.com>
2024-02-06 15:44:56 +0100

    consola netbeans

--- UD5/Hoja02/Ejercicio2/src/main/java/hoja02/ejercicio2/Ejercicio2.java
+++ UD5/Hoja02/Ejercicio2/src/main/java/hoja02/ejercicio2/Ejercicio2.java
@@ -12,16 +12,16 @@ public class Ejercicio2 {
 
     public static void main(String[] args) {
      Articulo [] ar= new Articulo[10]; 
-    for (int i=0; i<ar.length; i++){
-       if(i<(ar.length/2)){
+    for (int i=0; i<ar.length; i++){ // este recorre todo el array. Hace falta un contador para que solo muestre hasta donde hay datos. 
+       if(i<(ar.length/2)){ // i=0;i<contador;i++ 
            ar[i]=new Perecedero(2,2024,55, 25.5, "hola");
        } else{
            ar[i]=new EnPromocion(5.5,55,24.5,"hola");
        }
     } 
-    for (int i=0; i<ar.length; i++){
-        ar[i].datos();
-    }
+        for (Articulo ar1 : ar) {
+            ar1.datos();
+        }
      
     }
 }
